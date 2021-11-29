bb2cc68919bea1af9ee687d6e7b5adb07e3d582f
swjeno <81218141+swjeno@users.noreply.github.com>
2021-11-29 11:22:59 +0900

    Add files via upload

---
+++ ui 연결/improve.py
@@ -0,0 +1,41 @@
+from PyQt5.QtWidgets import *
+from PyQt5 import uic
+from PyQt5 import QtCore
+
+# UI 파일 연결
+# 단, UI파일은 Python코드 파일과 같은 디렉토리에 위치해야 한다.
+from_class = uic.loadUiType('improve.ui')[0]
+
+# 화면을 띄우는데 사용되는 class 선언
+class ImproveDialog(QDialog, from_class):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+
+        # 700*800 크기 고정
+        self.setFixedSize(900, 800)
+
+        # 버튼에 기능을 연결하는 코드
+        self.push_btn_number_setting.clicked.connect(self.buttonNumberSettingFunction)
+        self.push_btn_english_setting.clicked.connect(self.buttonEnglishSettingFunction)
+        self.push_btn_recognize.clicked.connect(self.buttonRecognizeFunction)
+        self.push_btn_close.clicked.connect(self.buttonCloseFunction)
+
+
+    def buttonNumberSettingFunction(self):
+        print("숫자 버튼이 눌렸습니다.")
+
+    def buttonEnglishSettingFunction(self):
+        print("영어 버튼이 눌렸습니다.")
+
+    def buttonRecognizeFunction(self):
+        print("재인식 버튼이 눌렸습니다.")
+
+    def buttonCloseFunction(self):
+        print("종료합니다.")
+        self.deleteLater()
+
+    def showModal(self):
+        return super().exec_()
+
+
---
+++ ui 연결/main_server.py
@@ -0,0 +1,92 @@
+from setting import *
+from improve import *
+from pic import *
+from video_cap import *
+from PyQt5 import QtGui
+import sys
+import socket_module
+
+from_class = uic.loadUiType('main.ui')[0]
+
+
+# 화면을 띄우는데 사용되는 class 선언
+class WindowClass(QMainWindow, from_class):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.initUI()
+
+        # 서버 소켓 생성
+        self.my_socket = socket_module.server_socket()
+
+    def initUI(self):
+        self.setFixedSize(700, 800)
+
+        # 언어변경
+        self.pushButton_engNum.clicked.connect(self.changeLang)
+
+        # 통화 연결
+        self.pushButton_call.clicked.connect(self.call)
+
+        # 설정
+        self.pushButton_setting.clicked.connect(self.settingButtonListener)
+
+        # 인식개선
+        self.pushButton_improve.clicked.connect(self.improveButtonListener)
+
+        # 지화보기
+        self.pushButton_picture.clicked.connect(self.pictureButtonListener)
+
+
+    # 언어변경
+    def changeLang(self):
+        self.my_video_cap.eng_num()
+        print('changeLang clicked')
+
+    # 통화 연결
+    def call(self):
+        print(self.my_video_cap.running)
+
+        if self.my_video_cap.running:
+            self.textEdit_me.clear()
+            self.cam_you.setPixmap(QtGui.QPixmap('stop.png'))
+            self.my_video_cap.stop()
+        else:
+            print('main.call (running state : false)')
+            self.my_video_cap.start()
+
+    # 설정
+    def settingButtonListener(self):
+        self.my_video_cap.stop()
+        win = SettingDialog()
+        win.showModal()
+
+        print('setting button clicked')
+
+    # 인식개선
+    def improveButtonListener(self):
+        self.my_video_cap.stop()
+        win = ImproveDialog()
+        win.showModal()
+
+        print('improve button clicked')
+
+    # 지화보기
+    def pictureButtonListener(self):
+        self.my_video_cap.stop()
+        win = PicDialog()
+        win.showModal()
+
+        print('pic button clicked')
+
+    def show(self):
+        super().show()
+        self.my_video_cap = video_cap(self.my_socket, self.cam_me, self.cam_you, self.textEdit_me, self.textEdit_you )
+        self.my_video_cap.start()
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    myWindow = WindowClass()
+    myWindow.show()
+    app.exec_()
